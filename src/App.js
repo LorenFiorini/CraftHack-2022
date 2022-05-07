400cff7e98a51ac596e91aa31414ae9ac84ee012
Chiemeka Madufor <61386343+elcozy@users.noreply.github.com>
2022-05-07 11:38:08 +0200

    added scss

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
-import React from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import GuideInfo from './components/GuideInfo'
-import Login from './components/Login'
-import UserInfo from './components/UserInfo'
-import TourBtn from './components/TourBtn'
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import GuideInfo from "./components/GuideInfo";
+import Login from "./components/Login";
+import UserInfo from "./components/UserInfo";
+import TourBtn from "./components/TourBtn";
+import "./style.scss";
 
 const App = () => {
-
-
-
   return (
     <Router>
-      <div>
+      <div className="tier">
         <Routes>
-          <Route path='/login' element={<Login/>}/>
-          <Route path='/guidinfo' element={<GuideInfo/>}/>
-          <Route path='/userinfo' element={<UserInfo/>} />
-          <Route path='/' element={<TourBtn/>} exact />
+          <Route path="/login" element={<Login />} />
+          <Route path="/guidinfo" element={<GuideInfo />} />
+          <Route path="/userinfo" element={<UserInfo />} />
+          <Route path="/" element={<TourBtn />} exact />
         </Routes>
       </div>
     </Router>
-  )
-}
+  );
+};
 
-export default App
+export default App;
